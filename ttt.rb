262ee3d68b2f4d872f2b3e82f7fbbc0ab828dd31
Kira McLean <kiraemclean@gmail.com>
2015-09-23 11:03:54 -0400

    Allow multiple calculations

---
+++ ttt.rb
@@ -0,0 +1,22 @@
+# pseudo code:
+
+# 1. label spots
+# 2. ask player to pick a spot
+# 3. computer picks a spot
+# 4. repeat until someone gets 3 in a row
+# bonus: try to get the computer to prefer 
+
+puts "Hi there! Welcome to tic tac toe. \n\n
+This is the game board: \n\n
+          |         |\n      
+     1    |    2    |    3\n
+          |         |\n
+----------+---------+---------\n
+          |         |\n       
+     4    |    5    |    6\n     
+          |         |\n   
+----------+---------+---------\n
+          |         |\n
+     7    |    8    |    9\n
+          |         |\n"
+
