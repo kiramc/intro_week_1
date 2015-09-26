dfdc96aee918862bd6a3bf2e48926cfbcccaaab1
Kira McLean <kiraemclean@gmail.com>
2015-09-26 16:35:27 -0400

    Finish dealers' turn

--- blackjack.rb
+++ blackjack.rb
@@ -1,22 +1,3 @@
-# 1. build a deck of cards
-# 2. player gets dealt 2, dealer gets dealt 2, store dealers total
-# 3. ask if player wants to hit or stay
-#   - if player hits:
-#       - if players total is > 21, player busts
-#       - if players total == 21, player wins
-#       - if plyers total < 21, repeat step 3
-#   - if player stays, store players total then switch to dealers turn
-# 4. check if dealer will hit or stay
-#   - if dealer total is < 17, dealer hits
-#     - if dealers new total < 17, dealer hits again
-#     - if dealers total ==  21, dealer wins
-#     - if dealers total > 21, dealer busts
-#   - if dealer total is > 17, dealer stays
-# 5. compare player and dealer totals
-#   - if player total > dealer total, player wins, else dealer wins
-#   - if player total == dealer total, it's a tie.
-
-
 def say(msg)
   puts "--> #{msg}"
 end
@@ -49,10 +30,6 @@ def show_cards(hand)
   hand.map{ |pair| "#{pair[1]}#{pair[0]}" }.join(", ")
 end
 
-# def hit(hand)
-#   hand << deck.pop
-# end
-
 say "Welcome to blackjack. What's your name?"
 players_name = gets.chomp
 
@@ -78,54 +55,78 @@ dealers_hand << deck.pop
 players_total = calculate_total(players_hand)
 dealers_total = calculate_total(dealers_hand)
 
-say "The dealer has: #{show_cards(dealers_hand)} for a total of #{dealers_total}"
+say "The dealer has: #{show_cards(dealers_hand)} for a total of #{dealers_total}."
 
 say "You have: #{show_cards(players_hand)} for a total of #{players_total}."
 
+if dealers_total == 21
+  say "Whoa that's lucky! Dealer hit blackjack, sorry. Game over."
+  exit  
+elsif players_total == 21
+  say "Whoa that's lucky! You hit blackjack! Congratulations, you win!"
+  exit
+end
+
 # Player's turn
 
-loop do
-
-  if dealers_total == 21
-    say "Whoa that's lucky! Dealer hit blackjack, game over."
-    exit  
-  elsif players_total == 21
-    say "Blackjack! Congratulations, you win!"
-    exit
-  elsif players_total > 21
-    say "Oh no! Looks like you busted, sorry. Maybe next time."
-    exit
-  else
-    say "Do you want to 1) hit or 2) stay?"
+while players_total < 21
+  say "Do you want to 1) hit or 2) stay?"
+  hit_or_stay = gets.chomp.to_i
+  loop do
+    break if hit_or_stay == 1 || hit_or_stay == 2
+    puts "That's an invalid selection. Pick 1 to hit, 2 to stay."
+    hit_or_stay = gets.chomp.to_i
   end
 
-  hit_or_stay = gets.chomp.to_i
-    loop do
-      break if hit_or_stay == 1 || hit_or_stay == 2
-      puts "That's an invalid selection. Pick 1 to hit, 2 to stay."
-      hit_or_stay = gets.chomp.to_i
-    end
-
-  # hit or stay
+  # hit
 
   if hit_or_stay == 1
     players_hand << deck.pop
     players_total = calculate_total(players_hand)
-    say "You chose hit, your new card is #{players_hand.last[1]}#{players_hand.last[0]}, for a total of #{players_total}"
+    say "You chose hit, your new card is #{players_hand.last[1]}#{players_hand.last[0]}, for a total of #{players_total}."
+    if players_total == 21
+      say "Blackjack! Congratulations, you win."
+      exit
+    elsif players_total > 21
+      say "Oh no! Looks like you busted, sorry. Maybe next time."
+      exit
+    end
   elsif hit_or_stay == 2
-    players_total = calculate_total(players_hand)
-    say "You chose to stay with a total of #{players_total}\n\n"
+    say "You chose to stay with a total of #{calculate_total(players_hand)}."
     break
-  end
+  end  
 end
 
 # Dealer's turn
 
-# loop do
-#   if dealers_total == 21
+say "Now it's the dealer's turn." if dealers_total < 17
 
+while dealers_total < 17
+  say "Dealing the dealer another card....."
+  dealers_hand << deck.pop
+  dealers_total = calculate_total(dealers_hand)
+  say "Dealer's new card is #{dealers_hand.last[1]}#{dealers_hand.last[0]}, for a total of #{dealers_total}."
+  if dealers_total == 21
+    say "Oh no! Dealer hit blackjack, sorry. Game over."
+    exit
+  elsif dealers_total > 21
+    say "Dealers busts! Congratulations, you win."
+    exit
+  end
+end
 
+# Compare hands
 
+say "Dealer's hand: #{show_cards(dealers_hand)}."
+say "Your hand: #{show_cards(players_hand)}."
+
+if players_total > dealers_total
+  say "Congratulations! You win."
+elsif players_total < dealers_total
+  say "Oh no, looks like dealer wins. Maybe next time."
+else
+  say "It's a tie!"
+end
 
 
 
